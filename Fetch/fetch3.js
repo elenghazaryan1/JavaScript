12b49bfdf996739a0f19c3b62e30eebd127a0dab
Elench⭐️ <elenaghazaryan31@gmail.com>
2026-09-04 16:50:01 +0400

    Practice fetch API manipulations

--- Fetch/fetch3.js
+++ Fetch/fetch3.js
@@ -1,41 +1,41 @@
-// // Names of products that costs less than $50
-// fetch("https://fakestoreapi.com/products")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((products) => {
-//     const price = products
-//       .filter((product) => product.price < 50)
-//       .map((product) => product.title);
+// Names of products that costs less than $50
+fetch("https://fakestoreapi.com/products")
+  .then((response) => {
+    return response.json();
+  })
+  .then((products) => {
+    const price = products
+      .filter((product) => product.price < 50)
+      .map((product) => product.title);
 
-//     console.log(price);
-//   });
+    console.log(price);
+  });
 
-// //The most expensive product
+//The most expensive product
 
-// fetch("https://fakestoreapi.com/products")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((products) => {
-//     const mostExpensive = products.reduce((max, product) => {
-//       return product.price > max.price ? product : max;
-//     });
-//     console.log(mostExpensive);
-//   });
+fetch("https://fakestoreapi.com/products")
+  .then((response) => {
+    return response.json();
+  })
+  .then((products) => {
+    const mostExpensive = products.reduce((max, product) => {
+      return product.price > max.price ? product : max;
+    });
+    console.log(mostExpensive);
+  });
 
-// Only products from the 'electronics category
+//Only products from the 'electronics category
 
-// fetch("https://fakestoreapi.com/products")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((products) => {
-//     const electronics = products.filter(
-//       (product) => product.category === "electronics",
-//     );
-//     console.log(electronics);
-//   });
+fetch("https://fakestoreapi.com/products")
+  .then((response) => {
+    return response.json();
+  })
+  .then((products) => {
+    const electronics = products.filter(
+      (product) => product.category === "electronics",
+    );
+    console.log(electronics);
+  });
 
 // Calculate the average price of all products
 
--- Fetch/fetch5.js
+++ Fetch/fetch5.js
@@ -1,23 +1,23 @@
-// // Prints only completed tasks
-// fetch("https://jsonplaceholder.typicode.com/todos?userId=3")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((tasks) => {
-//     const completed = tasks.filter((task) => task.completed === true);
-//     console.log(completed);
-//   });
-// // Prints only incomplete tasks
-// fetch("https://jsonplaceholder.typicode.com/todos?userId=3")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((tasks) => {
-//     const incomplete = tasks.filter((task) => task.completed === false);
-//     console.log(incomplete);
-//   });
+// Prints only completed tasks
+fetch("https://jsonplaceholder.typicode.com/todos?userId=3")
+  .then((response) => {
+    return response.json();
+  })
+  .then((tasks) => {
+    const completed = tasks.filter((task) => task.completed === true);
+    console.log(completed);
+  });
+// Prints only incomplete tasks
+fetch("https://jsonplaceholder.typicode.com/todos?userId=3")
+  .then((response) => {
+    return response.json();
+  })
+  .then((tasks) => {
+    const incomplete = tasks.filter((task) => task.completed === false);
+    console.log(incomplete);
+  });
 
-// // The total count of tasks and how many are completed
+// The total count of tasks and how many are completed
 
 fetch("https://jsonplaceholder.typicode.com/todos?userId=3")
   .then((response) => {
